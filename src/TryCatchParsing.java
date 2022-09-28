166a3f7edffbd667c998812cde47ceb87a20e733
dlinsmes <david.lin@smes.org>
2022-09-28 10:57:54 -0700

    try catch integer parsing

--- src/TryCatchParsing.java
+++ src/TryCatchParsing.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
 public class TryCatchParsing {
     public static void main(String [] args) {
-        Scanner s = new Scanner(System.in);
 
-        int num = -1;
-
-        boolean valid = false;
-        while(!valid) {
-            try{
-                System.out.println("enter a number between 1 and 10");
-                String inputString = s.nextLine();
-                num = Integer.parseInt(inputString);
-                if (num > 0 && num <= 10)
-                    valid = true;
-                else
-                    System.out.println("input out of range");
-            }
-            catch(Exception e) {
-                System.out.println("not a valid integer");
-            }
+        System.out.println("enter a number between 1 and 10");
+        int num = getNum();
+        while(!(num > 0 && num <= 10)) {
+            System.out.println("invalid");
+            num = getNum();
         }
-
         System.out.println("you entered " + num);
+    }
+
+    public static int getNum() {
+        Scanner s = new Scanner(System.in);
+        int num = -1;
+        try{
+            String inputString = s.nextLine();
+            num = Integer.parseInt(inputString);
+        }
+        catch(Exception e) {
+            System.out.println("not a number");
+        }
+        return num;
 
     }
 }
